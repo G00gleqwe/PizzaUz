427bc936023f256354f5fa09b58e81b421789cb2
Winchestor2001 <sbexruz2001@gmail.com>
2024-03-05 09:35:11 +0500

    update models

--- pizza/models.py
+++ pizza/models.py
@@ -29,6 +29,7 @@ class Order(models.Model):
     pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
     status = models.CharField(max_length=50, choices=STATUS)
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.user.username
